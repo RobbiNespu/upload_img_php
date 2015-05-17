0aaab136fd872a0cdd7f8c7fcab179183b2d5c68
Robbi Nespu <robbi.nespu@hotmail.com>
2015-05-17 15:18:14 +0800

    add database file

---
+++ database/temp_anis.sql
@@ -0,0 +1,47 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 17, 2015 at 09:16 AM
+-- Server version: 5.6.17
+-- PHP Version: 5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `temp_anis`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `upload_img`
+--
+
+CREATE TABLE IF NOT EXISTS `upload_img` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `url` varchar(25) NOT NULL,
+  `exp` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `upload_img`
+--
+
+INSERT INTO `upload_img` (`id`, `url`, `exp`) VALUES
+(1, 'url', '2015-05-01'),
+(2, '../images/1928710.png', NULL),
+(3, '../images/60403442.png', NULL);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
